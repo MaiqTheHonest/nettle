515933025d0aa8c385e3cf9830f443635e2a6ffd
MaiqTheHonest <rdemka@gmail.com>
2024-12-03 23:01:29 +0000

    nettle

---
+++ main.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+class KMeansClustering:
+
+    def __init__(self, k=3):
+        self.k = k
+        self.centroids = None 
+    
+    @staticmethod
+    def euclidean_distance(data_point, centroids):
+        return np.sqrt(np.sum((centroids - data_point)**2, axis=1))
+
+
+
+    def fit(self, X, max_iterations=200):   # X is data
+        self.centroids = np.random.uniform(np.amin(X, axis=0), np.amax(X, axis=0), 
+                                           size=(self.k, X.shape[1]))    # get the new centroids within dimension range
+        print(self.centroids)
+
+
+        for _ in range(max_iterations):
+            y = []
+
+            for data_point in X:
+                distances = KMeansClustering.euclidean_distance(data_point, self.centroids)
+                cluster_num = np.argmin(distances)   # finds index of the nearest (i.e. smallest distance) centroid
+                y.append(cluster_num)
+            
+            y = np.array(y)
+
+            cluster_indices = []
+
+            for i in range(self.k):
+                cluster_indices.append(np.argwhere(y == i))
+            
+            cluster_centers = []
+
+            for i, indices in enumerate(cluster_indices):
+                if len(indices) == 0:
+                    cluster_centers.append(self.centroids[i])
+                else:
+                    cluster_centers.append(np.mean(X[indices], axis=0)[0])
+
+            if np.max(self.centroids - np.array(cluster_centers)) < 0.0001:
+                print(y)
+                break
+            else:
+                self.centroids = np.array(cluster_centers)
+        
+        return y
+    
+
+test_data = np.random.randint(0, 100, (100, 2))
+
+kmeans = KMeansClustering(k=3)
+
+labels = kmeans.fit(test_data)
+test_data = np.vstack((test_data, kmeans.centroids))
+print(np.shape(test_data))
+labels = np.append(labels, np.zeros((kmeans.k, 1)))
+px.scatter(x=test_data[:, 0], y=test_data[:, 1], color=labels)
+#px.scatter(kmeans.centroids)
+#plt.scatter(test_data[:, 0], test_data[:, 1], c=labels)
+#plt.show()
