396943426585c35d3eb7434bf141f1fb4b5db7a9
MaiqTheHonest <rdemka@gmail.com>
2024-12-08 14:47:33 +0000

    mpl

--- main.py
+++ main.py
@@ -1,48 +1,21 @@
-from time import sleep
+
 import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
 from time import perf_counter
-
-
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
+from sklearn.datasets import make_blobs
 start_time = perf_counter()
 
+#test_data = np.random.randint(0, 100, (100, 2))
+test_data = np.concatenate([np.random.normal(0, 5, size=(200, 2)), 
+    np.random.normal(5, 3, size=(200, 2))]) # bimodal normal draw
 
-fig = go.FigureWidget()
-fig
-
-fig['layout'].update(height = 500, width = 600)
-
-
-test_data = np.random.randint(0, 100, (100, 2))
-
-trace1 = go.Scatter(
-    x=[],
-    y=[],
-    mode="markers",
-    name="points"
-)
-
-trace2 = go.Scatter(
-    x=[],
-    y=[],
-    mode="markers",
-    marker=dict(size=12, color="green", symbol="circle-x-open"),
-    name='centroids'
-)
-trace3 = go.Scatter(
-    x=[],
-    y=[],
-    mode="markers",
-    marker=dict(size=8, color="green", symbol="diamond"),
-    name='perfect centre'
-)
-
-fig.add_trace(trace1)
-fig.add_trace(trace2)
-fig.add_trace(trace3)
-
-
+# formatting
+fig = plt.figure()
+scatter1 = plt.scatter(test_data[:, 0], test_data[:, 1], label='points')
+scatter2 = plt.scatter([], [], c='red', marker="*", s=96,  label='centroids')
+scatter3 = plt.scatter([], [], c='green', label='perfect centre')
 
 class KMeansClustering:
 
@@ -56,20 +29,17 @@ class KMeansClustering:
     def euclidean_distance(data_point, centroids):
         return np.sqrt(np.sum((centroids - data_point)**2, axis=1))
     
-
-    def draw(self, labs=None):
-        
-        self.figure.update_traces(x=test_data[:, 0], y=test_data[:, 1], selector=dict(name="points"), marker=dict(color=labs))
-        self.figure.update_traces(x=self.centroids[:, 0], y=self.centroids[:, 1], selector=dict(name="centroids"), marker=dict(size=12, color="green", symbol="circle-x-open"))
-        self.figure.update_traces(x=[self.perfect_cent[0]], y=[self.perfect_cent[1]], selector=dict(name="perfect centre"), marker=dict(size=8, color="green", symbol="diamond"))
-        fig.show()
-        sleep(0.5)
+    def update_graph(self, labs=None):
+        scatter1.set_array(labs)
+        scatter2.set_offsets(self.centroids)
+        plt.pause(0.5)
+        plt.draw()
+        print(".")
 
     def fit(self, X, max_iterations=200): # X is data
         self.perfect_cent = [np.mean(X[:, 0]), np.mean(X[:, 1])]
         self.centroids = np.random.uniform(np.amin(X, axis=0), np.amax(X, axis=0), 
                                            size=(self.k, X.shape[1]))    # get the new centroids within dimension range
-
 
         for _ in range(max_iterations):
             y = []
@@ -100,12 +70,11 @@ class KMeansClustering:
                 self.centroids = np.array(cluster_centers)
             
             
-            self.draw(labs=y)
+            self.update_graph(labs=y)
             
         return y
 
-kmeans = KMeansClustering(k=5, figure=fig)
-
-labels = kmeans.fit(test_data)
-
-print(f"script finished in {perf_counter() - start_time} seconds")
+if __name__ == "__main__":
+    kmeans = KMeansClustering(k=5, figure=fig)
+    labels = kmeans.fit(test_data)
+    print(f"script finished in {perf_counter() - start_time} seconds")
