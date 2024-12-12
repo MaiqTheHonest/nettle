b63a1a7d64b84dccec64d1a7d20a6bd0d8f18975
MaiqTheHonest <rdemka@gmail.com>
2024-12-12 12:55:01 +0000

    _

--- TC_no_numpy.py
+++ TC_no_numpy.py
@@ -1,35 +1,44 @@
-import math
+from math import sqrt
 from time import perf_counter
 timer1 = perf_counter()
 
 
 
-positions = [[1,5],[3,2],[7,3]]
-center_x, center_y = [7,7]
-k=1
+#points = [[1,5],[3,2],[7,3]]
 
-def TC_dist_to_points(x,y):
-    res=0
-    for i in positions:
-        a, b = i
-        res+=math.sqrt((x-a)**2+(y-b)**2)
-    res+= k*math.sqrt((x-center_x)**2+(y-center_y)**2)
-    return res
+def weighted_distance(points, center, k) -> list:
 
-inix, iniy = 0,0
-cur=TC_dist_to_points(inix, iniy)
-step=1
-while step > 0.000001:
-    f=False
-    for dx, dy in [(-1,0), (1,0), (0,1), (0, -1)]:
-        curx, cury = inix+step*dx, iniy+ step*dy
-        if TC_dist_to_points(curx, cury)<cur:
-            cur=TC_dist_to_points(curx, cury)
-            inix, iniy = curx, cury
-            f=True
-        
-    if f==False:
-        step = step/10
-print(cur, [curx, cury])
-timer2 = perf_counter()
-print(timer2 - timer1)
+    center_x, center_y = center
+
+    def TC_dist_to_points(x,y):
+        res=0
+        for point in points:
+            a, b = point
+            res+=sqrt((x-a)**2+(y-b)**2)                # sums the point-centroid differences
+        res+= k*sqrt((x-center_x)**2+(y-center_y)**2)   # sums the centroid-center difference
+        return res
+
+    step=1
+    inix, iniy = 0,0
+
+    TC_now = TC_dist_to_points(inix, iniy)
+
+    while step > 0.000001:
+        f=False
+        for dx, dy in [(-1,0), (1,0), (0,1), (0, -1)]:
+            curx, cury = inix+step*dx, iniy+ step*dy
+            if TC_dist_to_points(curx, cury)<TC_now : 
+                TC_now = TC_dist_to_points(curx, cury)
+                inix, iniy = curx, cury
+                f=True
+            
+        if f==False:
+            step = step/10
+
+    return [curx, cury, TC_now]
+
+#print(weighted_distance(points=points, center=[7,7], k=1))
+
+# print(TC_now ,  [curx, cury])
+# timer2 = perf_counter()
+# print(timer2 - timer1)
